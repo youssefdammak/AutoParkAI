5a65131d3284cd97bbdb825d5a719e3c34b26f73
youssef dammak <youssedbenmaherdammak@gmail.com>
2025-03-04 23:26:29 -0500

    added the plate number extraction and writing it to an excel file

--- plateDetection/main.py
+++ plateDetection/main.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 import cv2
 from sort.sort import*
-from util import get_car
+from util import*
+
+results={}
 
 #load tracker
 mot_tracker=Sort()
@@ -26,6 +28,7 @@ while ret:
     ret,frame=cap.read()
     
     if ret and frame_nmr<10:
+        results[frame_nmr]={}
         #detect vehicles
         detections=coco_model(frame)[0]
         detections_=[]
@@ -52,8 +55,15 @@ while ret:
             license_plate_crop_gray=cv2.cvtColor(license_plate_crop,cv2.COLOR_BGR2GRAY)
             _,license_plate_crop_thresh=cv2.threshold(license_plate_crop_gray,64,255,cv2.THRESH_BINARY_INV)
 
-            cv2.imshow('original_crop',license_plate_crop)
-            cv2.imshow('threshold',license_plate_crop_thresh)
-
-            cv2.waitKey(0)
+            #read license plate number
+            license_plate_text,license_plate_text_score=read_license_plate(license_plate_crop_thresh)
             
+            if license_plate_text is not None:
+                results[frame_nmr][car_id]={'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                            'license_plate': {'bbox': [x1, y1, x2, y2],
+                                            'text': license_plate_text,
+                                            'bbox_score': score,
+                                            'text_score': license_plate_text_score}}
+                
+            # write results
+            write_csv(results, r'E:\AutoParkAI\plateDetection\test.csv')
